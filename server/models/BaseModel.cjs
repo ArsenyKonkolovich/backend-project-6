f2d8893e2d53f4eabd2cf6c1f906fe9f8190659e
thanatos <seniaslams@gmail.com>
2022-12-13 16:04:19 +0500

    Add comments

--- server/models/BaseModel.cjs
+++ server/models/BaseModel.cjs
@@ -3,6 +3,8 @@
 const { Model } = require('objection');
 
 module.exports = class BaseModel extends Model {
+  // Класс BaseModel расширяет класс Model, добавляя в него новый статический геттер
+// modelPaths, которая отдает в массиву ссылку на папку, гле лежит BaseModel
   static get modelPaths() {
     return [__dirname];
   }
