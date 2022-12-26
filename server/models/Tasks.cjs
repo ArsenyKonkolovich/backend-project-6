405eea5ec0bd2d4f3b98fec2593284d28b66a747
Арсений Конколович <333@onlinepbx.ru>
2022-12-26 14:43:04 +0500

    Add error on empty status id

--- server/models/Tasks.cjs
+++ server/models/Tasks.cjs
@@ -18,7 +18,7 @@ module.exports = class Tasks extends unique(BaseModel) {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
         description: { type: 'string' },
-        statusId: { type: 'integer' },
+        statusId: { type: 'integer', minimum: 1 },
         creatorId: { type: 'integer' },
       },
     };
